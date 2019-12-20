5b677237e2983d23e68e34d670407b26c7a4c969
Vikash Kumar Choubey <VikashKumar.Choubey@veritas.com>
2019-12-20 19:29:04 +0530

    Update to play game.
    Add following things:
    1. Getting random cells for putting numbers.
    2. Rotation of array so that we can reduce code redundancy.
    3. Will try to make board size variable.

--- Design2488.py
+++ Design2488.py
@@ -1,12 +1,16 @@
-from random import seed
+from numpy import rot90
 from random import choice
 from random import randint
+from random import seed
+import time
+
+BOARD_SIZE = 4
 
 class game2488:
 	def __init__(self):
 		self.matrix = []
-		for _ in range(4):
-			self.matrix.append([0]*4)
+		for _ in range(BOARD_SIZE):
+			self.matrix.append([0] * BOARD_SIZE)
 
 		self.move_dict = {
 			1: self.move_right,
@@ -16,36 +20,57 @@ class game2488:
 		}
 
 		self.fill_random_two_position()
+		print("Initial board:")
+		print(self.matrix)
+
+	def get_empty_cell(self):
+		empty_cell_list = []
+		for i in range(BOARD_SIZE):
+			for j in range(BOARD_SIZE):
+				if self.matrix[i][j] == 0:
+					empty_cell_list.append([i,j])
+
+		return empty_cell_list
 
 	def fill_random_two_position(self):
 		"""
 		This method will fill random 2 position of our matrix.
 		"""
+		empty_cell_list = self.get_empty_cell()
+
 		for _ in range(2):
-			i = randint(0,3)
-			j = randint(0,3)
-			self.matrix[i][j] = choice([2,4])
+			idx = randint(0,len(empty_cell_list) - 1)
+			self.matrix[empty_cell_list[idx][0]][empty_cell_list[idx][1]] = 2
+			del empty_cell_list[idx]
 
 	def play_game(self):
 		# Will decide break point here.
 		while True:
-			current_move = randint(1,4)
-			self.move_dict[current_move]()
-			# Get the cell left in matrix
-			# If cell left is one and matrix before current_move 
-			# and after move are same then we will end the game
+			current_move = randint(0, 3)
+			print("Current Move: {}".format(current_move))
 
-	def get_cell_left(self):
-		pass
+			# Rotate and call move_left
+			self.matrix = rot90(self.matrix, current_move)
+			self.move_left()
+			self.matrix = rot90(self.matrix, -current_move)
+
+			# Add new value to board
+			empty_cell_list = self.get_empty_cell()
+			idx = randint(0,len(empty_cell_list) - 1)
+			self.matrix[empty_cell_list[idx][0]][empty_cell_list[idx][1]] = 2
+
+			print(self.matrix)
+			time.sleep(5)
+
 
 	# Move methods
 	def move_right(self):
 		pass
 
-
 	def move_left(self):
+		current_zero = -1
 		for i in range(4):
-			# Move all int to left
+			# Move all int to move_left
 			for j in range(4):
 				if self.matrix[i][j] == 0 and current_zero == -1:
 					current_zero = j
@@ -63,6 +88,7 @@ class game2488:
 					self.matrix[i][j + 1] = 0
 
 			# Move all int to left
+			current_zero = -1
 			for j in range(4):
 				if self.matrix[i][j] == 0 and current_zero == -1:
 					current_zero = j
